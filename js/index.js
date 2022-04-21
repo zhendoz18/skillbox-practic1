2a85c480bee11acc30b4e4b61a09ab30c11f649e
zhendoz18 <84764655+zhendoz18@users.noreply.github.com>
2022-04-21 15:43:52 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -267,3 +267,39 @@ new JustValidate('.contacts__validate', {
   }
 });
 
+// подключение яндекс-карты
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init(){
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [55.758468, 37.601088],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 14
+    });
+
+    // Создание геообъекта с типом точка (метка).
+    var myGeoObject = new ymaps.GeoObject({
+        geometry: {
+            type: "Point", // тип геометрии - точка
+            coordinates: [55.758468, 37.601088] // координаты точки
+        }
+    });
+
+    var myPlacemark = new ymaps.Placemark([55.758468, 37.601088], {}, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/contacts-point-img.svg',
+      iconImageSize: [20, 20],
+      iconImageOffset: [-3, -42]
+    });
+
+    // Размещение геообъекта на карте.
+    // myMap.geoObjects.add(myGeoObject);
+    myMap.geoObjects.add(myPlacemark);
+}
